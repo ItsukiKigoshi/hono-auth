ed52ade81dfae13ad0ac40e5b148fa3b4fb2bdbd
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-03-24 21:43:14 -0700

    feat: add funcs debug

--- apps/app/functions/api/[[route]].ts
+++ apps/app/functions/api/[[route]].ts
@@ -1,8 +1,9 @@
+/*
 import app from '../../../api/src/index'
 import {createAuth} from "../../../api/src/lib/auth";
 import { handle } from 'hono/cloudflare-pages'
 
-app.on(["POST", "GET"], "/api/auth/*", async (c) => {
+app.on(["POST", "GET"], "/api/auth/!*", async (c) => {
     if (!c.env || !c.env.BETTER_AUTH_SECRET) {
         console.error("Critical: Environment variables are missing!");
         return c.json({ error: "Server environment not configured" }, 500);
@@ -12,4 +13,6 @@ app.on(["POST", "GET"], "/api/auth/*", async (c) => {
     return auth.handler(c.req.raw);
 });
 
-export const onRequest = handle(app)
+export const onRequest = handle(app)*/
+
+export const onRequest = () => new Response("I am Alive!");
