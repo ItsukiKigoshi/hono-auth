7d261d550f2967c7163f55a09e9913d0ee1ce779
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-03-24 23:11:59 -0700

    feat: why?わいわceDンうおcmlいiiitwuamqieyaemaieidi

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
     try {
       const { error } = await authClient.signIn.magicLink({
         email,
-        callbackURL: "/",
+        callbackURL: "https://hono-auth.pages.dev/",
       });
 
       if (error) {
