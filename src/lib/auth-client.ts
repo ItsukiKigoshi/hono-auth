0d476f26b03c818c1e1c793ea120a8da95a300b3
Itsuki Kigoshi <itsukikigoshi@gmail.com>
2026-03-24 18:08:55 -0700

    initialise repo うざたん

--- src/lib/auth-client.ts
+++
@@ -1,8 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-import { passkeyClient } from "@better-auth/passkey/client";
-import { magicLinkClient } from "better-auth/client/plugins";
-
-export const authClient = createAuthClient({
-  baseURL: "https://hono-auth.pages.dev",
-  plugins: [passkeyClient(), magicLinkClient()],
-});
--- src/lib/auth-generation.ts
+++
@@ -1,3 +0,0 @@
-import { getAuth } from "./auth-server";
-
-export const auth = getAuth({} as Env);
